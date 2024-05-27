08136497b07393fd1276eb1ce3ab1f4db032ea9c
Kei Ishikawa <k.stoneriv@gmail.com>
2024-05-27 19:15:00 +0900

    add script for apt install

---
+++ misc.sh
@@ -0,0 +1,8 @@
+#  install requirements
+sudo apt update
+sudo apt install vim tmux htop fish xclip
+
+# setup git user info
+git config --global user.email "k.stoneriv@gmail.com"
+git config --global user.name "Kei Ishikawa"
+
