e48ae8bb550d25f659b8d97cda94ff55d9dc2e53
Kei Ishikawa <k.stoneriv@gmail.com>
2024-05-28 11:28:46 +0900

    add ctags to install script

--- misc.sh
+++ misc.sh
@@ -1,6 +1,6 @@
 #  install requirements
 sudo apt update
-sudo apt install vim tmux htop fish xclip
+sudo apt install vim tmux htop fish xclip exuberant-ctags
 
 # setup git user info
 git config --global user.email "k.stoneriv@gmail.com"
