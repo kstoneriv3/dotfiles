07f006ebeadae1d0d74376f034c7c55fa7882d3d
Kei Ishikawa <k.stoneriv@gmail.com>
2024-11-21 18:08:50 +0900

    feat: add btop and tldr to misc.sh

--- misc.sh
+++ misc.sh
@@ -1,6 +1,6 @@
 #  install requirements
 sudo apt update
-sudo apt install vim tmux htop fish xclip exuberant-ctags
+sudo apt install vim tmux btop fish xclip exuberant-ctags tldr
 
 # setup git user info
 git config --global user.email "k.stoneriv@gmail.com"
